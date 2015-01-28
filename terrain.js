c13922c04ae4791e726307ce01da0df1453b6c2d
unknown <gelles@OFFICE-SERVER.Yoosh.local>
2015-01-28 14:24:34 +0200

    ga-pages

--- terrain.js
+++ terrain.js
@@ -183,9 +183,9 @@ function generateTexture(data, width, height) {
 	    
 	    imageData[i + 2] = 100; 
 	}
-	else if (!bigdata[j] || bigdata[j] > 30) {
+	else if (!bigdata[j] || bigdata[j] > 33) {
 	    imageData[i] = (46 + shade * 128) * (0.5 + data[j] * 0.007);
-	    imageData[i + 1] = (46 + shade * 128) * (0.5 + data[j] * 0.007);
+	    imageData[i + 1] = 100;
 	    imageData[i + 2] = 0;	    
 	}
 	else {
