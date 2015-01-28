db9bce772409c0a690b6fef08f0fd02a2f1d8b38
unknown <gelles@OFFICE-SERVER.Yoosh.local>
2015-01-28 17:51:14 +0200

    no random

--- terrain.js
+++ terrain.js
@@ -56,7 +56,7 @@ function init() {
     scene = new THREE.Scene();
 
     
-    controls = new THREE.FirstPersonControls(camera);
+    controls = new THREE.FlyPersonControls(camera);
     controls.movementSpeed = 800;
     controls.lookSpeed = 0.1;
 
