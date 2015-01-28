acde2fc20754c32aa9558d26839a846dfab9aa87
unknown <gelles@OFFICE-SERVER.Yoosh.local>
2015-01-28 17:57:13 +0200

    fly

--- terrain.js
+++ terrain.js
@@ -56,7 +56,7 @@ function init() {
     scene = new THREE.Scene();
 
     
-    controls = new THREE.FlyControls(camera);
+    controls = new THREE.TrackballControls(camera);
     controls.movementSpeed = 800;
     controls.lookSpeed = 0.1;
 
