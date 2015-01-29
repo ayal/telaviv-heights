5ef97f9e55f571fc0e45b8d69338b78b882de511
ayal <ayalgelles@gmail.com>
2015-01-29 19:34:39 +0200

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -2,7 +2,7 @@
 
 var nwest = new google.maps.LatLng(32.097765, 34.743147)
 var mapOptions = {
-    zoom: 16,
+    zoom: 1,
     center: nwest
 }
 map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
